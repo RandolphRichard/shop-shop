2fa75e0203d9b6678d26447b57b88deffb2fee0d
Randolph Richard <randolphrichard9@gmail.com>
2022-01-19 08:13:39 -0500

    set the stripe functionality allign with the cart

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,7 +1,7 @@
-import { idbPromise } from "../../utils/helpers";
 import React from 'react';
 import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
@@ -13,6 +13,7 @@ const CartItem = ({ item }) => {
       _id: item._id
     });
     idbPromise('cart', 'delete', { ...item });
+
   };
 
   const onChange = (e) => {
@@ -22,17 +23,18 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id
       });
-    
       idbPromise('cart', 'delete', { ...item });
+
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
-    
       idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+
     }
+  }
 
   return (
     <div className="flex-row">
